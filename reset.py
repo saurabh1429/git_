86d24c5d6ef9c67a67b0c1a16135bb3c14e16cef
Saurabh Ojha <saurabhojha1418@gmail.com>
2024-09-20 23:49:19 -0700

    addedreser-paswword

---
+++ reset.py
@@ -0,0 +1,8 @@
+username = input("enter user name ")
+password = input("enter pass =")
+
+if username == "Admin" and password == "Admin":
+    print("you can reset")
+
+else:
+    print("you can not reset")
